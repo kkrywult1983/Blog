03d41f8a87d39c0a340d90d2c9588523ae8ed1ad
Kamil Sobik <kamil.sobik@gmail.com>
2023-01-30 20:22:38 +0100

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import PostsList from "./pages/PostsList";
+import PostsList from "./pages/PostsList.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
